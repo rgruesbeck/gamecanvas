9296696878be5cab0aee67d5bdc8c4c559767128
Ron Gruesbeck <rongruesbeck@gmail.com>
2019-07-05 15:59:37 -0700

    update mobile bundle

--- mobilebundle.js
+++ mobilebundle.js
@@ -1,21 +1,6 @@
 // Mobile fixes
 
-// prevent refresh on pulls
-function preventPullToRefresh(element) {
-    var prevent = false;
-    document.querySelector(element).addEventListener('touchstart', function (e) {
-        if (e.touches.length !== 1) {
-            return;
-        }
-        var scrollY = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
-        prevent = (scrollY === 0);
-    });
-    document.querySelector(element).addEventListener('touchmove', function (e) {
-        if (prevent) {
-            prevent = false;
-            e.preventDefault();
-        }
-    });
-}
-
-preventPullToRefresh('#app');
+// prevent scroll/refresh
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, { passive: false }) 
